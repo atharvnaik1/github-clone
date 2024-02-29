975dc6c8c0ecf4700eaad394d98d57a441837c4e
test0o0 <atharv9951@gmail.com>
2024-02-29 23:27:22 +0530

    user controller resolved

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,6 @@
 import User from "../models/user.model.js";
 
-export const getUserProfileAndRepos = async (req, res) => {
+export const getUserProfileAndRepos = async (req,res) => {
 	const { username } = req.params;
 	try {
 		// 60 requests per hour, 5000 requests per hour for authenticated requests
@@ -11,13 +11,29 @@ export const getUserProfileAndRepos = async (req, res) => {
 			},
 		});
 
+		if (!userRes.ok) {
+            throw new Error(`Failed to fetch user profile: ${userRes.status} ${userRes.statusText}`);
+        }
+
 		const userProfile = await userRes.json();
+
+		
+		if (!userProfile.repos_url) {
+            throw new Error("User profile does not contain a repos_url property.");
+        }
+
+		
+
 
 		const repoRes = await fetch(userProfile.repos_url, {
 			headers: {
 				authorization: `token ${process.env.GITHUB_API_KEY}`,
 			},
 		});
+		if (!repoRes.ok) {
+            throw new Error(`Failed to fetch user repositories: ${repoRes.status} ${repoRes.statusText}`);
+        }
+
 		const repos = await repoRes.json();
 
 		res.status(200).json({ userProfile, repos });
